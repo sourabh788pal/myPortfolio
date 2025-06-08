964de55e2f0df662d7b6c7dfa77a0e121ee5a04e
sourabh788pal <palsourabh788@gmail.com>
2025-06-08 09:06:43 +0530

    Add files via upload
    js file for hamburger and show header option on click

---
+++ index.js
@@ -0,0 +1,9 @@
+
+  const hamburger = document.getElementById("hamburger");
+  const navLinks = document.getElementById("nav-links");
+
+  hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    navLinks.classList.toggle("active");
+  });
+
